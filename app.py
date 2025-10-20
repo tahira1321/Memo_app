d36b39bc7b8ba88da108a4ccaf2611421f56b4c1
tahira1321 <takuya.hirano.321+git@gmail.com>
2025-10-21 01:41:28 +0900

    Memoクラス作成

--- app.py
+++ app.py
@@ -8,6 +8,18 @@ app = Flask(__name__)
 
 # 4. クラスの定義 (CRUD関数がこのクラスに依存するため、先に定義)
 
+# 4-1. メモの情報を管理するクラス
+class Memo:
+  # コンストラクタを定義し、インスタンス生成時に属性を初期化
+  def __init__(self, title, content, create_date):
+    self.title = title                # メモのタイトル
+    self.content = content            # メモの内容
+    self.create_date = create_date    # メモ作成日
+  
+  # 後続のステップでデバッグに便利なメソッドを定義しておく
+  def __repr__(self):
+    return f"Memo(Title='{self.title}', Content='{self.content}')"
+
 # 5. CRUD/データ操作関数
 
 # 6. データベース初期化の実行
